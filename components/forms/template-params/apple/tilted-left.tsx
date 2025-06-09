997d19e32ec4bd84f7ebd67f60e3f7b90c26eff9
Ahmed Kasem <ahmed.a.kasem18@gmail.com>
2025-06-09 20:21:29 +0300

    remove logo from tilt-left template

--- components/forms/template-params/apple/tilted-left.tsx
+++ components/forms/template-params/apple/tilted-left.tsx
@@ -100,60 +100,6 @@ export function Form() {
             </div>
 
             <div className="grid w-full items-center gap-1.5">
-              <Label htmlFor="logo">Logo</Label>
-              <div className="flex space-x-2 overflow-hidden">
-                <div className="min-w-0 flex-1">
-                  <ImageSelector
-                    id="logo"
-                    onChange={(v) =>
-                      template.updateParams({
-                        logo: {
-                          ...(params.logo ?? {}),
-                          url: v ?? "",
-                        },
-                      })
-                    }
-                    initialFileName={
-                      params.logo.url
-                        ? params.logo.url.split("/").pop()
-                        : undefined
-                    }
-                  />
-                </div>
-                <ResponsivePopover
-                  title="Logo Settings"
-                  description="Customize the logo."
-                  trigger={
-                    <Button variant="outline" size="icon">
-                      <MixerHorizontalIcon className="h-4 w-4" />
-                    </Button>
-                  }
-                >
-                  <ImageSettings
-                    width={params.logo.width ?? 200}
-                    height={params.logo.height ?? 200}
-                    onChangeWidth={(width) =>
-                      template.updateParams({
-                        logo: {
-                          ...params.logo,
-                          width: width ?? 200,
-                        },
-                      })
-                    }
-                    onChangeHeight={(height) =>
-                      template.updateParams({
-                        logo: {
-                          ...params.logo,
-                          height: height ?? 200,
-                        },
-                      })
-                    }
-                  />
-                </ResponsivePopover>
-              </div>
-            </div>
-
-            <div className="grid w-full items-center gap-1.5">
               <Label htmlFor="screenshot">Screenshot</Label>
               <ImageSelector
                 id="screenshot"
