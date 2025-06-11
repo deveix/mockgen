a3aef3bdb8105fdd47b267c41bb02777b34a95ec
Ahmed Kasem <ahmed.a.kasem18@gmail.com>
2025-06-11 23:23:16 +0300

    add clarity

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css"
 import type { Metadata } from "next"
 import Image from "next/image"
 import Link from "next/link"
+import Script from "next/script"
 import { MultiTemplateStoreProvider } from "@/providers/multi-template-store-provider"
 import { TemplateStoreProvider } from "@/providers/template-store-provider"
 import { Analytics } from "@vercel/analytics/react"
@@ -248,6 +249,15 @@ export default async function RootLayout({
 
         <SpeedInsights />
         <Analytics />
+        <Script id="clarity-script" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "${process.env.NEXT_PUBLIC_CLARITY_PROJECT_ID}");
+          `}
+        </Script>
       </body>
     </html>
   )
